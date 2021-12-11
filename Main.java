5f6534ec3ed1bb966d099f0901e783ad26d098d8
ajaniSterling <ajani.sterling@hgs.hiddengeniusproject.org>
2021-12-11 02:47:33 +0000

    Developed 3.2 The if else if Statement

---
+++ Main.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;    // Needed for the Scanner class
+
+/**
+ *    This program determine grade for a test score.
+ */
+public class Main
+{
+   public static void main(String[] args)
+   {
+      int  score;    // To hold a test score
+      char grade;    // To hold a letter grade
+
+      // Create a Scanner object to read input.
+      Scanner console = new Scanner(System.in);
+
+      // Get the test score.
+      System.out.print("Enter your numeric test score : ");
+      score = console.nextInt();
+
+      // Calculate the grade.
+      if (score >= 90)
+      {
+         grade = 'A';
+      }
+      else if (score >= 80)
+      {
+         grade = 'B';
+      }
+      else if (score >= 70)
+      {
+         grade = 'C';
+      }
+      else if (score >= 50)
+      {
+         grade = 'D';
+      }
+      else
+      {
+         grade = 'F';
+      }
+
+      // Display the grade.
+      System.out.println("Your grade is " + grade);
+   }
+}
